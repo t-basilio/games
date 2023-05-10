ae55ac0947b97f3ed8f6d65f80ad3dfb1da26156
Thiago Basilio <basilio.barboza@gmail.com>
2023-05-10 01:04:49 -0300

    GET game by id

--- src/main/java/com/basoft/games/controllers/GameController.java
+++ src/main/java/com/basoft/games/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.basoft.games.controllers;
 
+import com.basoft.games.dto.GameDTO;
 import com.basoft.games.dto.GameMinDTO;
 import com.basoft.games.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping("/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
--- src/main/java/com/basoft/games/services/GameService.java
+++ src/main/java/com/basoft/games/services/GameService.java
@@ -1,9 +1,12 @@
 package com.basoft.games.services;
 
+import com.basoft.games.dto.GameDTO;
 import com.basoft.games.dto.GameMinDTO;
+import com.basoft.games.entities.Game;
 import com.basoft.games.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById( Long gameId) {
+        Game game = gameRepository.findById(gameId).get();
+        return new GameDTO(game);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
     }
